4e610ddf90b20fd4834483272048daf279558fb0
marlon <marlon.guo@outlook.com>
2025-02-19 17:48:17 +0800

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   // 開發中、產品路徑
-  base: process.env.NODE_ENV === "production" ? "/hex_react_week2/" : "/",
+  // hex_react_template 改成專案名稱
+  base: process.env.NODE_ENV === "production" ? "/hex_react_template/" : "/",
   plugins: [react()],
 });
